5ba4986d599aeafee70808466b0027e8cc874f56
Ubuntu <ubuntu@ip-172-26-13-216.ap-south-1.compute.internal>
2024-06-06 19:45:17 +0530

    updated

--- dist/ba_root/mods/files/settings.py
+++
@@ -1,8 +0,0 @@
-Reply_on_cmd = True
-Reply_on_cmd_type = 'Screen' # SHould me "Screen" or "Chat"
-cmd_prefix = '/'
-hide_cmd = True
-whitelist = False
-whitelist_pb = False
-Show_map_text = True
-map_text = [u'\ue062VORTEX\ue062', u'\ue04bVORTEX x HONOR\ue04b', u'\ue048HONOR\ue048', u'\ue04bVORTEX x HONOR\ue04b', u'\ue062VORTEX\ue062', u'\ue048HONOR\ue048']
--- dist/ba_root/mods/tools/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 8.5.1 (trial), 000000, 2024-03-27T20:48:06.018811
-from .pyarmor_runtime import __pyarmor__
